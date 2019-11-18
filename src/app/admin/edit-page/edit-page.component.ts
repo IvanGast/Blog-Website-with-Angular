4c90342ce562789071717e8495ad3d75eb38a575
Ivan <ivan.gastilovic@hotmail.com>
2019-11-18 16:04:22 +0200

    Initial commit

---
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
+import { PostService } from "src/app/shared/post.service";
+import { switchMap } from "rxjs/operators";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Post } from "src/app/shared/interfaces";
+
+@Component({
+  selector: "app-edit-page",
+  templateUrl: "./edit-page.component.html",
+  styleUrls: ["./edit-page.component.scss"]
+})
+export class EditPageComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostService
+  ) {}
+
+  ngOnInit() {
+    this.route.params
+      .pipe(
+        switchMap((params: Params) => {
+          return this.postsService.getById(params["id"]);
+        })
+      )
+      .subscribe((post: Post) => {
+        this.form = new FormGroup({
+          title: new FormControl(post.title, Validators.required),
+          text: new FormControl(post.text, Validators.required)
+        });
+      });
+  }
+
+  submit() {}
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import { Environment } from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: "AIzaSyDT33XVzg5qYF3Zr7uA24mXc7fft40zWAs",
+  fbDbUrl: "https://angular-blog-9095c.firebaseio.com"
+};
